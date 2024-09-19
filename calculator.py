45b5c8c19a9ec3a7ce2715bd1da6b530382894ab
OwenC7642 <owensethcooper@gmail.com>
2024-09-19 09:34:12 -0400

    I made a calcualtor

---
+++ calculator.py
@@ -0,0 +1,8 @@
+# Basic Calculator
+
+print("Hey Fam, Welcome to my bussin calculator!")
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+answer = num1 + num2 
+print(f"Your answer is: {answer}!!!")
